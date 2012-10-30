b9aaa6a1f7fa8bea9996306085e499ac20b3b44b
Ken Wakita <wakita@is.titech.ac.jp>
2012-10-30 11:51:30 +0900

    U クラスは、頻繁に利用するユーティリティメソッドを登録したクラスです。

---
+++ src/lib/U.java
@@ -0,0 +1,42 @@
+package lib;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+public class U {
+    public static final InputStream in = System.in;
+    public static final PrintStream out = System.out, err = System.err;
+
+    public static <V> List<V> arrayList() {
+        return new ArrayList<V>();
+    }
+
+    public static <V> Set<V> treeSet() {
+        return new TreeSet<V>();
+    }
+
+    public static <V> Set<V> hashSet() {
+        return new HashSet<V>();
+    }
+
+    public static <K, V> Map<K, V> hashMap() {
+        return new HashMap<K, V>();
+    }
+
+    public static <K, V> Map<K, V> treeMap() {
+        return new TreeMap<K, V>();
+    }
+    
+    private static long timerT;
+    public static void timerStart() {
+        timerT = System.currentTimeMillis(); 
+    }
+    
+    public static long timerStop() {
+        return System.currentTimeMillis() - timerT;
+    }
+
+    private U() {
+    }
+}
